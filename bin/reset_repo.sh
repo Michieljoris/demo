4dba344e0a4627c0885d2628950397b05bb0570b
Michiel van Oosten <mail@axion5.net>
2015-08-06 02:58:21 +1000

    Sample demo.json and init file

---
+++ bin/reset_repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pwd
+REPO=$1
+BRANCH=$2
+DB=$REPO"_"$BRANCH
+echo 'Resetting '$DB
+echo 'Preparing sql dump file'
+sed  "s/chin_minimal/$DB/g" < ~/chin_minimal.sql  >~/db.sql
+echo 'Importing sql dump'
+mysql -u root -pmypwd < ~/db.sql
+cd ~/repos/$REPO/branches/$BRANCH
+echo 'Now in '`pwd`
+echo 'Executing rake db:migrate'
+DATABASE_URL=mysql2://root:mypwd@localhost/$DB; rake db:migrate
